b815215e7dc0e0b752fdfe7cc0260d6b8ca7c975
MarioWork <mariovieira.work@hotmail.com>
2023-10-07 01:59:12 +0000

    feat: :construction_worker: created a accordion component

---
+++ src/components/common/accordion-component.vue
@@ -0,0 +1,40 @@
+<template>
+    <button @click.prevent="onClickHandler" class="header">
+        {{ title }}
+    </button>
+    <div v-if="showContent" class="content">
+        <slot></slot>
+    </div>
+</template>
+
+<script setup>
+    import { toRefs, ref } from 'vue';
+
+    const showContent = ref(false);
+
+    const props = defineProps({
+        title: { type: String, required: true }
+    });
+
+    const { title } = toRefs(props);
+
+    const onClickHandler = () => (showContent.value = !showContent.value);
+</script>
+
+<style scoped>
+    .header {
+        width: 100%;
+        background-color: gray;
+        color: white;
+        font-size: 1rem;
+        text-align: left;
+        border: none;
+        padding: 1em 1em 1em 0.5em;
+        cursor: pointer;
+    }
+
+    .content {
+        background-color: gray;
+        padding: 5em;
+    }
+</style>

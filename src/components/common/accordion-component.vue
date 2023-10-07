4f4dbf0c9785a591cbc3610c960570e27761d111
MarioWork <mariovieira.work@hotmail.com>
2023-10-07 02:49:18 +0000

    feat: :construction_worker: continue working on accordion css

--- src/components/common/accordion-component.vue
+++ src/components/common/accordion-component.vue
@@ -1,6 +1,8 @@
 <template>
     <button @click.prevent="onClickHandler" class="header">
         {{ title }}
+        <ChevronDown v-if="!showContent" class="icon"></ChevronDown>
+        <ChevronUp v-else class="icon"></ChevronUp>
     </button>
     <div v-if="showContent" class="content">
         <slot></slot>
@@ -8,6 +10,8 @@
 </template>
 
 <script setup>
+    import ChevronDown from '../common/icons/chevron-down.vue';
+    import ChevronUp from '../common/icons/chevron-up.vue';
     import { toRefs, ref } from 'vue';
 
     const showContent = ref(false);
@@ -22,19 +26,25 @@
 </script>
 
 <style scoped>
+    .icon {
+        margin-left: auto;
+        width: 1.5rem;
+    }
     .header {
         width: 100%;
         background-color: gray;
         color: white;
         font-size: 1rem;
         text-align: left;
-        border: none;
+        border: 0;
         padding: 1em 1em 1em 0.5em;
         cursor: pointer;
+        display: flex;
+        align-items: center;
     }
 
     .content {
         background-color: gray;
-        padding: 5em;
+        padding: 0.5em;
     }
 </style>

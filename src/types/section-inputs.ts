2c4a8e00b8e398a113ce317f6fcfb24aa8e2674d
MarioWork <mariovieira.work@hotmail.com>
2023-10-28 23:56:41 +0000

    feat: :zap: create a types file for sections inputs and a enum for sections names

---
+++ src/types/section-inputs.ts
@@ -0,0 +1,9 @@
+export enum infoInputNames {
+    FIRST_NAME = 'firstName',
+    LAST_NAME = 'lastName',
+    NATIONALITY = 'nationality',
+    BIRTHDAY = 'birthday',
+    PHONE = 'phone',
+    EMAIL = 'email',
+    LOCATION = 'location'
+}

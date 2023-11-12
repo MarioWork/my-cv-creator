1b44b26efbef82eee59e0d66143f9a0d216d2ef8
MarioWork <mariovieira.work@hotmail.com>
2023-11-12 19:52:01 +0000

    continue working on work exp

--- src/components/sidebar/sidebar-sections/work-experience.vue
+++ src/components/sidebar/sidebar-sections/work-experience.vue
@@ -1,7 +1,42 @@
 <template>
-    <div></div>
+    <Accordion title="Work Experience">
+        <div class="container">
+            <CustomInput
+                v-for="{ name, type } in inputs"
+                :key="name"
+                :name="name"
+                :type="type"
+                @on-change="updateField"
+            />
+        </div>
+    </Accordion>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+    import { inputTypes } from '../../../types/input-types';
+    import { workExperienceInputNames } from '../../../types/section-inputs';
+
+    import Accordion from '../../common/accordion-component.vue';
+    import CustomInput from '../../common/input-component.vue';
+
+    import { useCV } from '../../../composables/use-cv';
+
+    const { updateField } = useCV();
+
+    const inputs = [
+        {
+            name: workExperienceInputNames.start,
+            type: inputTypes.TEXT
+        },
+        {
+            name: workExperienceInputNames.end,
+            type: inputTypes.TEXT
+        },
+        {
+            name: workExperienceInputNames.title,
+            type: inputTypes.TEXT
+        }
+    ];
+</script>
 
 <style scoped></style>
